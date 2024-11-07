d6112a13b6b08a7ec98fa462d120ff38ca673761
서가영 <alstpgml0812@gmail.com>
2024-11-07 17:33:24 +0900

    Feat: TodoService update

--- todo-api/src/main/java/com/example/todo_api/todo/TodoService.java
+++ todo-api/src/main/java/com/example/todo_api/todo/TodoService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
@@ -31,8 +33,37 @@ public class TodoService {
     }
 
     // 할 일 조회 (특정 멤버의 모든 할 일 조회)
+    @Transactional(readOnly = true)
+    public List<Todo> getAllTodo(Long memberId) throws Exception{
+        Member member = memberRepository.findById(memberId);
+
+        if (member == null) {
+            throw new Exception("존재하지 않는 멤버입니다.");
+        }
+
+        return todoRepository.findAllByMember(member);
+    }
 
     // 할 일 수정
+    @Transactional
+    public void updateTodo(Long todoId, String newContent, Long memberId) throws Exception {
+        Todo todo = todoRepository.findById(todoId);
+        Member member = memberRepository.findById(memberId);
+
+        if (todo == null) {
+            throw new Exception("존재하지 않는 할 일 입니다.");
+        }
+
+        if (member == null) {
+            throw new Exception("존재하지 않는 멤버입니다.");
+        }
+
+        if (todo.getMember() != member) {
+            throw new Exception("다른 유저의 할 일을 수정할 수 없습니다.");
+        }
+
+        todo.updateContent(newContent);
+    }
 
     // 할 일 삭제
 }
